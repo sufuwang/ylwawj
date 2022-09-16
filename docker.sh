dba1c67b9b250870e3fa2e27a1e3cd337158c617
sufuwang <1137120948@qq.com>
2022-09-16 14:12:55 +0800

    feat:测试workflow

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 docker rmi nginx-docker
-docker build -t nginx-docker -f ./nginx.dockerfile .
-docker run -p 8080:80 nginx-docker
+docker build -t sufuwang/ylwawj-fe -f ./nginx.dockerfile .
+# docker run -p 8080:80 nginx-docker
